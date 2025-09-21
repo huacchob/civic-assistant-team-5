9d49113d1990a67d4836df5b627e40660b47ecd1
huacchob <59075675+huacchob@users.noreply.github.com>
2025-09-21 17:27:31 -0400

    Carlos (#42)
    * updating a last few type annotations and README
    
    * updating tests
    
    * updating tests
    
    * fixing github actions
    
    * adding secrets to github actions

--- utility/secrets.py
+++ utility/secrets.py
@@ -7,6 +7,9 @@ from pathlib import Path
 def load_secrets() -> None:
     """Load secrets into environment variables."""
     creds_file: Path = Path(__file__).parent.parent.joinpath(".env")
+    if not creds_file.exists():
+        print("No .env file found.")
+        return
     with open(file=creds_file, mode="r", encoding="utf-8") as file:
         lines: list[str] = file.readlines()
         for line in lines:
